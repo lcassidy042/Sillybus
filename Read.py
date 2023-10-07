92202c5c2779ad31f79d2ea1028c7c3e86a72f76
frogchewer <yyaragarla@gmail.com>
2023-10-07 14:15:06 -0400

    Working Through Read

--- Read.py
+++ Read.py
@@ -4,5 +4,10 @@ file_path = "Syllabi/test.txt"
 
 file = open(file_path)
 lines = file.readlines()
-for line in lines:
-    print(line)
+length = len(lines)
+i = 0
+while i<length :
+
+    words = lines[i].split(" ")
+    print(words)
+    i += 1
